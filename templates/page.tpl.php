d75e8c1f498fff6a84a5a29f08340ed3b071de9a
himerus <jake.strawn@gmail.com>
2011-01-07 16:10:59 +0000

    adding codebase for Gamma v1

---
+++ templates/page.tpl.php
@@ -0,0 +1,32 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Theme implementation to display a single Drupal page.
+ */
+?>
+<div id="page" class="clearfix">
+  <?php if (isset($zones_above)): ?>
+  <div id="zones-above" class="clearfix"><?php print $zones_above; ?></div>
+  <?php endif; ?>
+  
+  <div id="zones-content">
+    <div id="zones-content-inner" class="clearfix">
+      <div id="zones-content-background" class="clearfix">
+        <?php if (isset($messages)): ?>
+        <div id="message-container" class="container-<?php print $default_container_width; ?> clearfix">
+          <div class="grid-<?php print $default_container_width; ?>">
+            <?php print $messages; ?>
+          </div>
+        </div><!-- /.container-xx -->
+        <?php endif; ?>
+        <?php print $content_zone; ?>
+      </div>
+    </div>
+  </div>
+  
+  <?php if (isset($zones_below)): ?>
+  <div id="zones-below" class="clearfix"><?php print $zones_below; ?></div>
+  <?php endif; ?>
+</div><!-- /#page -->
---
+++ theme-settings.php
@@ -0,0 +1,19 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Theme settings for the Omega theme.
+ */
+
+/**
+ * Implements hook_form_system_theme_settings_alter().
+ *
+ * @param $form
+ *   Nested array of form elements that comprise the form.
+ * @param $form_state
+ *   A keyed array containing the current state of the form.
+ */
+function gamma_form_system_theme_settings_alter(&$form, &$form_state) {
+  // Include any changes to the theme settings here.
+}
